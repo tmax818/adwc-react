531b2dfebc5d55c728e967af2873002c8461bec3
tmax818 <tylermaxwell661@gmail.com>
2020-01-24 20:38:19 -0800

    Updates

---
+++ precache-manifest.0a94b441ed57f6457c885d92b6fe7072.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21f4897cc177eb3534c1dfc664c3a631",
+    "url": "/adwc-react/index.html"
+  },
+  {
+    "revision": "70679a8a9119f76f1f05",
+    "url": "/adwc-react/static/css/main.f062fd30.chunk.css"
+  },
+  {
+    "revision": "bc8f59603ad57b5d3a15",
+    "url": "/adwc-react/static/js/2.a6c45601.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/adwc-react/static/js/2.a6c45601.chunk.js.LICENSE"
+  },
+  {
+    "revision": "70679a8a9119f76f1f05",
+    "url": "/adwc-react/static/js/main.11280b66.chunk.js"
+  },
+  {
+    "revision": "4412ecc8686abe4ae7f7",
+    "url": "/adwc-react/static/js/runtime-main.cd47eb97.js"
+  },
+  {
+    "revision": "63aa645d789cd854a23697ce8e659c52",
+    "url": "/adwc-react/static/media/bg-nav.63aa645d.jpg"
+  },
+  {
+    "revision": "067187ee4e3d0c307c49d8b48b0067d4",
+    "url": "/adwc-react/static/media/bg-top.067187ee.jpg"
+  },
+  {
+    "revision": "43dc3fafce025b319e610ffd49e27a01",
+    "url": "/adwc-react/static/media/logo.43dc3faf.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/adwc-react/precache-manifest.0a94b441ed57f6457c885d92b6fe7072.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/adwc-react/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
